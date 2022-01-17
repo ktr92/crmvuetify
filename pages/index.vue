caf39e128f159123b2b79c2ed5f101f3725e67cf
ktr92 <kudashevtim@gmail.com>
2022-01-17 16:55:16 +0300

    master

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,54 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <v-data-table
+      :headers="[]"
+      :items="orders"
+      :items-per-page="5"
+      class="elevation-1"
+    >
+     <template v-slot:header="" >
+      <thead>
+        <tr>
+          <th colspan="1">Имя</th>
+          <th :colspan="orderscount">Category 2</th>
+        </tr>
+      </thead>
+       </template>
+      <template v-slot:item="props">
+        <tr>
+           <td>{{ props.item.name }}</td>
+           <td v-for="prop in props.item.cart" :key="prop.label">
+             {{ prop.count }}
+           </td>
+          
+        </tr>
+      </template>
+    </v-data-table>
+  </div>
 </template>
+
+
+<script>
+import AppOrders from '@/components/AppOrders.vue'
+export default {
+  components: { AppOrders },
+  asyncData({store}) {
+    const orders = store.getters['orders/orders']
+    return {orders}
+  },
+  data() {
+    return {
+      orders: {},
+      headers: [
+       
+      ]
+    }
+  },
+ 
+  computed: {
+    orderscount: function() {
+      return this.orders[0].cart.length
+    }
+  }
+}
+</script>

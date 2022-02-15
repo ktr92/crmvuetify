b0d78eab553cb4a60a0aff233eadfe054f666c36
ktr92 <kudashevtim@gmail.com>
2022-02-15 17:44:50 +0300

    make it better

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,7 +65,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.BASEURL || 'http://localhost:3000'
+    baseURL: process.env.NODE_ENV ? process.env.BASEURL : 'http://localhost:3000'
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
